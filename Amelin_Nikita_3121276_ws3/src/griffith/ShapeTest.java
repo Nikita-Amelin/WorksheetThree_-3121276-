4a52f515f43b8222ee619ec3796be9d1b9ac8d96
Nikita Amelin <nikita.amelin@student.griffith.ie>
2025-03-19 20:13:52 +0000

    integration testing implementation

--- Amelin_Nikita_3121276_ws3/src/griffith/ShapeTest.java
+++ Amelin_Nikita_3121276_ws3/src/griffith/ShapeTest.java
@@ -2,6 +2,8 @@ package griffith;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -9,6 +11,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ShapeTest {
+	
+	//Creating objects for tesing:
 	Circle c = new Circle(5);
 	Rhombus r = new Rhombus(6, 8);
 	RightAngledTriangle t = new RightAngledTriangle(6, 8);
@@ -34,4 +38,48 @@ public class ShapeTest {
 		assertNotNull(t.toString());
 	}
 	
+	@Test
+	public void test() {
+		ArrayList<Shape> shapes = new ArrayList<Shape>();
+		shapes.add(new Circle(10));
+		shapes.add(new Circle(2));
+		shapes.add(new Rhombus(2, 9));
+		shapes.add(new Rhombus(4, 12));
+		shapes.add(new RightAngledTriangle(5, 8));
+		shapes.add(new RightAngledTriangle(2, 10));
+		
+		double[] areas = {314.159, 12.56636, 9, 24, 20, 10};
+		double[] perimeters = {62.8318, 12.56636, 18.439088914585774, 25.298221281347036, 22.4339811320566, 22.19803902718557};
+
+		for(int i=0; i<shapes.size(); i++) {
+			assertEquals(areas[i], shapes.get(i).area(), 0.0001);
+			assertEquals(perimeters[i], shapes.get(i).perimeter(), 0.0001);
+		}
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
