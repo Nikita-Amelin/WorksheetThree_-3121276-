fd9935e3cdc06bdfdb6875f180af070d6e62ca19
Nikita Amelin <nikita.amelin@student.griffith.ie>
2025-03-19 18:35:56 +0000

    Creating test case

---
+++ Amelin_Nikita_3121276_ws3/src/griffith/ShapeTest.java
@@ -0,0 +1,29 @@
+package griffith;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ShapeTest {
+
+	
+	@Test
+	public void perimeterTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void areaTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void toStringTest() {
+		fail("Not yet implemented");
+	}
+	
+}
